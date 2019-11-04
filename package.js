ee552af3bd90ba9aff5ef6ae0050612cac90d695
Ben Lavalley <ben.lavalley@kaseya.com>
2019-11-04 06:42:20 -0500

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorspark:util',
   summary: 'Bring some of NodeJS util functionality to the Meteor client',
-  version: '0.2.0',
+  version: '0.2.1',
   git: 'https://github.com/MeteorSpark/util'
 });
 
@@ -10,7 +10,7 @@ server = 'server';
 client = 'client';
 
 Package.onUse(function (api) {
-  api.versionsFrom('0.9.4');
+  api.versionsFrom('1.8.1');
 
   api.add_files('lib/util-client.js', client);
   api.add_files('lib/util-server.js', server);
